91fbb6c1c0d4a8e50be0fe2c6a62eae23a97439f
Makrand Gupta <makrand1996@gmail.com>
2016-07-14 14:22:25 -0400

    Migrated constants to dedicated file

---
+++ android/app/src/main/java/com/kiimobiletech/makrand/featuresheetgenerator/Constants.java
@@ -0,0 +1,9 @@
+package com.kiimobiletech.makrand.featuresheetgenerator;
+
+/**
+ * Created by Makrand Gupta on 2016-07-14.
+ */
+public class Constants {
+    public final static String EXTRA_PAYLOAD = "com.kiimobiletech.makrand.featuresheetgenerator.FILLED";
+    public final static String GENERATED_FILE = "generated.html";
+}
