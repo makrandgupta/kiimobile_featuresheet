1cf376937366af07deee07fc7cdb836c65b986f7
Makrand Gupta <makrand1996@gmail.com>
2016-07-18 16:21:07 -0400

    added missing files

--- android/app/src/main/java/com/kiimobiletech/makrand/featuresheetgenerator/DataInputActivity.java
+++ android/app/src/main/java/com/kiimobiletech/makrand/featuresheetgenerator/DataInputActivity.java
@@ -1,12 +1,7 @@
 package com.kiimobiletech.makrand.featuresheetgenerator;
 
-import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
-
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.support.design.widget.TabLayout;
-//import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -24,7 +19,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.IOException;
 
 public class DataInputActivity extends AppCompatActivity implements AgentInfoFragment.OnNextListener, PropertyInfoFragment.OnNextListener{
 
@@ -52,6 +48,7 @@ public class DataInputActivity extends AppCompatActivity implements AgentInfoFra
 
     FloatingActionButton fab;
     DataContainer dataContainer = DataContainer.getInstance();
+    Generator generator = Generator.getInstance();
 
 
     @Override
@@ -74,94 +71,32 @@ public class DataInputActivity extends AppCompatActivity implements AgentInfoFra
         assert tabLayout != null;
         tabLayout.setupWithViewPager(mViewPager);
 
-//        fab = (FloatingActionButton) findViewById(R.id.fab);
-//        if(fab != null)
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Will generate document in future", Snackbar.LENGTH_LONG)
-//                        .setAction("Dismiss", new View.OnClickListener() {
-//                            //TODO: Check if this actually works. If not then go for alternate implementation.
-//                            //currently assuming that an empty click will automatically dismiss the snackbar
-//                            //alternate makes final object from the snackbar and then calls the dismiss() method on it
-//                            //http://stackoverflow.com/questions/30729312/how-to-dismiss-a-snackbar-using-its-own-action-button
-//                            @Override
-//                            public void onClick(View v) {}
-//                        })
-//                        .show();
-//            }
-//        });
-//        fab.hide();
-
-        findViewById(R.id.pink_icon).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(DataInputActivity.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.setter);
-        button.setSize(FloatingActionButton.SIZE_MINI);
-        button.setColorNormalResId(R.color.pink);
-        button.setColorPressedResId(R.color.pink_pressed);
-        button.setIcon(R.drawable.ic_fab_star);
-        button.setStrokeVisible(false);
-
-        final View actionB = findViewById(R.id.action_b);
-
-        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
-        actionC.setTitle("Hide/Show Action above");
-        actionC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
-            }
-        });
-
-        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
-        menuMultipleActions.addButton(actionC);
-
-        final FloatingActionButton removeAction = (FloatingActionButton) findViewById(R.id.button_remove);
-        removeAction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((FloatingActionsMenu) findViewById(R.id.multiple_actions_down)).removeButton(removeAction);
-            }
-        });
-
-        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
-        drawable.getPaint().setColor(getResources().getColor(R.color.white));
-        ((FloatingActionButton) findViewById(R.id.setter_drawable)).setIconDrawable(drawable);
-
-        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
-        actionA.setOnClickListener(new View.OnClickListener() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        if(fab != null)
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actionA.setTitle("Action A clicked");
+                try {
+                    generator.saveDocument();
+                } catch (Generator.TemplateException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                Snackbar.make(view, "Will generate document in future", Snackbar.LENGTH_LONG)
+                        .setAction("Dismiss", new View.OnClickListener() {
+                            //TODO: Check if this actually works. If not then go for alternate implementation.
+                            //currently assuming that an empty click will automatically dismiss the snackbar
+                            //alternate makes final object from the snackbar and then calls the dismiss() method on it
+                            //http://stackoverflow.com/questions/30729312/how-to-dismiss-a-snackbar-using-its-own-action-button
+                            @Override
+                            public void onClick(View v) {}
+                        })
+                        .show();
             }
         });
-
-        // Test that FAMs containing FABs with visibility GONE do not cause crashes
-        findViewById(R.id.button_gone).setVisibility(View.GONE);
-
-        final FloatingActionButton actionEnable = (FloatingActionButton) findViewById(R.id.action_enable);
-        actionEnable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                menuMultipleActions.setEnabled(!menuMultipleActions.isEnabled());
-            }
-        });
-
-        FloatingActionsMenu rightLabels = (FloatingActionsMenu) findViewById(R.id.right_labels);
-        FloatingActionButton addedOnce = new FloatingActionButton(this);
-        addedOnce.setTitle("Added once");
-        rightLabels.addButton(addedOnce);
-
-        FloatingActionButton addedTwice = new FloatingActionButton(this);
-        addedTwice.setTitle("Added twice");
-        rightLabels.addButton(addedTwice);
-        rightLabels.removeButton(addedTwice);
-        rightLabels.addButton(addedTwice);
+        fab.hide();
     }
 
     @Override
@@ -192,9 +127,8 @@ public class DataInputActivity extends AppCompatActivity implements AgentInfoFra
         if(position < 3) {
             tabLayout.getTabAt(position + 1).select();
         }
-        if(dataContainer.completed()) {
-//            fab.show();
-        }
+        if(dataContainer.completed())
+            fab.show();
     }
 
 
