4d3c10b2153899875fcd4c58239115589ec6e18a
Makrand Gupta <makrand1996@gmail.com>
2016-07-20 15:05:37 -0400

    added more constants

--- android/app/src/main/java/com/kiimobiletech/makrand/featuresheetgenerator/Constants.java
+++ android/app/src/main/java/com/kiimobiletech/makrand/featuresheetgenerator/Constants.java
@@ -8,4 +8,9 @@ public class Constants {
     public final static String GENERATED_FILE = "generated.html";
     public final static String TEMP_TEMPLATE = "template.html";
     public final static int NUM_TABS = 3;
+    public static final int SELECT_PHOTO = 100;
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String ARG_NUM_IMAGES = "num_images";
+    public static final String SOURCE_GALLERY = "gallery";
+    public static final String SOURCE_CAMERA = "camera";
 }
